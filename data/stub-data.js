642a3a88507f6e9a757838df3c9e65100a059347
Gilberto Duran <gduran.info@gmail.com>
2021-03-12 15:47:25 -0600

    Adding some stubs

--- data/stub-data.js
+++ data/stub-data.js
@@ -4,7 +4,7 @@ const stubNotes = [
   {
     id: uuidv4(),
     name: 'Learn Webpack',
-    status: 'to-do',
+    status: 'doing',
   },
   {
     id: uuidv4(),
@@ -15,7 +15,27 @@ const stubNotes = [
     id: uuidv4(),
     name: 'Learn Angular',
     status: 'to-do',
-  }
+  },
+  {
+    id: uuidv4(),
+    name: 'Learn Vue',
+    status: 'to-do',
+  },
+  {
+    id: uuidv4(),
+    name: 'Learn Java',
+    status: 'doing',
+  },
+  {
+    id: uuidv4(),
+    name: 'Learn ASP .NET',
+    status: 'to-do',
+  },
+  {
+    id: uuidv4(),
+    name: 'Learn JavaScript',
+    status: 'done',
+  },
 ];
 
 module.exports = { stubNotes } ;
