cdbfb9031dceec30d7dca8d7798b33324462743a
Alan <Alan.AlvarengaMejia@telus.com>
2021-03-13 19:25:30 -0600

    updating documentation

--- app/index.js
+++ app/index.js
@@ -10,13 +10,17 @@ const app = document.getElementById('add-task');
 const todoContainer = document.getElementById('to-do');
 const doingContainer = document.getElementById('doing');
 const doneContainer = document.getElementById('done');
-
+/**
+ * Update each lane with the updated information
+ */
 const renderTasks = () => {
   todo.loadList(data, todoContainer);
   doing.loadList(data, doingContainer);
   done.loadList(data, doneContainer);
 }
-
+/**
+ * Add move ability to all task across the lanes in sequential order
+ */
 const addEventsTasks = () => {
   const todoTasks = document.querySelectorAll('#to-do input[type="checkbox"]');
   todoTasks.forEach(todoItem =>
@@ -28,6 +32,12 @@ const addEventsTasks = () => {
   );
 }
 
+/**
+ * auxiliary method to update data status
+ * this one will be removed in future verions
+ * @param id
+ * @param status
+ */
 const updateStatus = (id, status) => {
   for (let i = 0; i < data.length; i++) {
     if (data[i].id === id) {
@@ -37,18 +47,23 @@ const updateStatus = (id, status) => {
   renderApp();
 }
 
+/**
+ * Display the app to the user
+ */
 const renderApp = () => {
   renderTasks();
   addEventsTasks();
 }
 
+// Add new task form to the DOM
 task.addForm(app);
+// Append "New Task" button to the form
 app.appendChild(task.addButton);
-
+// Add ability to append new task on to-do queue
 task.addButton.addEventListener('click', () => todo.addTask(
   document.getElementById("task-name"),
   data,
   todoContainer
 ));
-
+// let's go!
 renderApp();
